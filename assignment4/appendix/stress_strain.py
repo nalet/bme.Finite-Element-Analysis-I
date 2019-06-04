bc62cb17f38f051733f3651fda067903de434b73
Nalet Meinen <nalet.meinen@outlook.com>
2019-06-04 22:35:18 +0200

    Finished reporting Assignment 4 Appendix

--- assignment4/appendix/stress_strain.py
+++ assignment4/appendix/stress_strain.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 import sys
 
 plt.rcParams['figure.dpi'] = 120
-plt.rcParams['font.size'] = 15
+plt.rcParams['font.size'] = 9
 
 
-h = 10
-step_size = 0.0001
+
 
 sigma_start = 0
-sigma_step = 10e6
-sigma0 = 220e6
-B = 3e9
-E = 70e9
+sigma_step = 10
+sigma0 = 220
+B = 3e3
+E = 70e3
 n = 3.2
 
-w = 275
-b = 30
+w = 20
+b = 0.030 / 2 # symmetric
+h = 0.010
 
 x = []
 y = []
@@ -48,9 +48,12 @@ while c_x <= 1:
     print('{:.4e}'.format(c_y),'{:.4e}'.format(c_x),sep="\t")
     
 def read_data(filename) :
-    # Import data using Pandas. Using report I XY data, this line should work
-    data = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + '/' + filename,
-                    skiprows=5, header=None, delim_whitespace=True)
+    try:
+        # Import data using Pandas. Using report I XY data, this line should work
+        data = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + '/' + filename,
+                        skiprows=5, header=None, delim_whitespace=True)
+    except:
+        print('File ' + filename + ' not found')
     data = data.values
 
     x_f = [0]
@@ -61,7 +64,7 @@ def read_data(filename) :
         u2 = abs(line[2]) # displacement
     
         epsilon2 = np.log(h/(h-u2))
-        sigma2 = rf2 / w * b
+        sigma2 = rf2 / ( w * b )
 
         epsilon_bar = 2 / np.sqrt(3) * epsilon2
         sigma_bar = np.sqrt(3) / 2 * sigma2
@@ -75,12 +78,31 @@ def read_data(filename) :
     return [x_f, y_f]
 
 no_fillet_friction_less_finite_sliding_surface_to_surface = read_data("no_fillet_friction_less_finite_sliding_surface_to_surface.rpt")
+no_fillet_friction_less_finite_sliding_node_to_surface = read_data("no_fillet_friction_less_finite_sliding_node_to_surface.rpt")
+no_fillet_friction_less_small_sliding_surface_to_surface = read_data("no_fillet_friction_less_small_sliding_surface_to_surface.rpt")
+no_fillet_friction_less_small_sliding_node_to_surface = read_data("no_fillet_friction_less_small_sliding_node_to_surface.rpt")
 
+no_fillet_friction_02_finite_sliding_surface_to_surface = read_data("no_fillet_friction_02_finite_sliding_surface_to_surface.rpt")
+no_fillet_friction_02_finite_sliding_node_to_surface = read_data("no_fillet_friction_02_finite_sliding_node_to_surface.rpt")
+no_fillet_friction_02_small_sliding_surface_to_surface = read_data("no_fillet_friction_02_small_sliding_surface_to_surface.rpt")
+no_fillet_friction_02_small_sliding_node_to_surface = read_data("no_fillet_friction_02_small_sliding_node_to_surface.rpt")
 
 # Plot
 plt.grid()
 plt.plot(x, y, linewidth=1, label="Analytical Solution")
-plt.plot(no_fillet_friction_less_finite_sliding_surface_to_surface[0],no_fillet_friction_less_finite_sliding_surface_to_surface[1], linewidth=1, label="Finite Sliding - Surface to Surface - No Friction")
+
+
+plt.plot(no_fillet_friction_less_finite_sliding_node_to_surface[0],no_fillet_friction_less_finite_sliding_node_to_surface[1], linewidth=1, label="friction less - finite sliding - surface to surface")
+plt.plot(no_fillet_friction_less_finite_sliding_surface_to_surface[0],no_fillet_friction_less_finite_sliding_surface_to_surface[1], linewidth=1, label="friction less - finite sliding - node to surface")
+plt.plot(no_fillet_friction_less_small_sliding_surface_to_surface[0],no_fillet_friction_less_small_sliding_surface_to_surface[1], linewidth=1, label="friction less - small sliding - node to surface")
+plt.plot(no_fillet_friction_less_small_sliding_node_to_surface[0],no_fillet_friction_less_small_sliding_node_to_surface[1], linewidth=1, label="friction less - small sliding - node to surface")
+
+plt.plot(no_fillet_friction_02_finite_sliding_surface_to_surface[0],no_fillet_friction_02_finite_sliding_surface_to_surface[1], linewidth=1, label="with friction - finite sliding - surface to surface")
+plt.plot(no_fillet_friction_02_finite_sliding_node_to_surface[0],no_fillet_friction_02_finite_sliding_node_to_surface[1], linewidth=1, label="with friction - finite sliding - node to surface")
+plt.plot(no_fillet_friction_02_small_sliding_surface_to_surface[0],no_fillet_friction_02_small_sliding_surface_to_surface[1], linewidth=1, label="with friction - small sliding - node to surface")
+plt.plot(no_fillet_friction_02_small_sliding_node_to_surface[0],no_fillet_friction_02_small_sliding_node_to_surface[1], linewidth=1, label="with friction - small sliding - node to surface")
+
+
 plt.xlabel('Strain')
 plt.ylabel('Stress (Pa)')
 plt.title('No Fillet')
@@ -88,3 +110,36 @@ plt.ylim(bottom=0)
 plt.legend()
 plt.show()
 
+
+fillet_friction_less_finite_sliding_surface_to_surface = read_data("fillet_friction_less_finite_sliding_surface_to_surface.rpt")
+fillet_friction_less_finite_sliding_node_to_surface = read_data("fillet_friction_less_finite_sliding_node_to_surface.rpt")
+fillet_friction_less_small_sliding_surface_to_surface = read_data("fillet_friction_less_small_sliding_surface_to_surface.rpt")
+fillet_friction_less_small_sliding_node_to_surface = read_data("fillet_friction_less_small_sliding_node_to_surface.rpt")
+
+fillet_friction_02_finite_sliding_surface_to_surface = read_data("fillet_friction_02_finite_sliding_surface_to_surface.rpt")
+fillet_friction_02_finite_sliding_node_to_surface = read_data("fillet_friction_02_finite_sliding_node_to_surface.rpt")
+fillet_friction_02_small_sliding_surface_to_surface = read_data("fillet_friction_02_small_sliding_surface_to_surface.rpt")
+fillet_friction_02_small_sliding_node_to_surface = read_data("fillet_friction_02_small_sliding_node_to_surface.rpt")
+
+# Plot
+plt.grid()
+plt.plot(x, y, linewidth=1, label="Analytical Solution")
+
+
+plt.plot(fillet_friction_less_finite_sliding_node_to_surface[0],fillet_friction_less_finite_sliding_node_to_surface[1], linewidth=1, label="friction less - finite sliding - surface to surface")
+plt.plot(fillet_friction_less_finite_sliding_surface_to_surface[0],fillet_friction_less_finite_sliding_surface_to_surface[1], linewidth=1, label="friction less - finite sliding - node to surface")
+plt.plot(fillet_friction_less_small_sliding_surface_to_surface[0],fillet_friction_less_small_sliding_surface_to_surface[1], linewidth=1, label="friction less - small sliding - node to surface")
+plt.plot(fillet_friction_less_small_sliding_node_to_surface[0],fillet_friction_less_small_sliding_node_to_surface[1], linewidth=1, label="friction less - small sliding - node to surface")
+
+plt.plot(fillet_friction_02_finite_sliding_surface_to_surface[0],fillet_friction_02_finite_sliding_surface_to_surface[1], linewidth=1, label="with friction - finite sliding - surface to surface")
+plt.plot(fillet_friction_02_finite_sliding_node_to_surface[0],fillet_friction_02_finite_sliding_node_to_surface[1], linewidth=1, label="with friction - finite sliding - node to surface")
+plt.plot(fillet_friction_02_small_sliding_surface_to_surface[0],fillet_friction_02_small_sliding_surface_to_surface[1], linewidth=1, label="with friction - small sliding - node to surface")
+plt.plot(fillet_friction_02_small_sliding_node_to_surface[0],fillet_friction_02_small_sliding_node_to_surface[1], linewidth=1, label="with friction - small sliding - node to surface")
+
+
+plt.xlabel('Strain')
+plt.ylabel('Stress (Pa)')
+plt.title('Fillet')
+plt.ylim(bottom=0)
+plt.legend()
+plt.show()
